c31bb68932313253cb4bb18a341e69fe5806dadd
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-10-04 16:34:01 +0900

    [Init] Initialize Repository

---
+++ carrier_sherpa/src/main/java/com/sherpa/carrier_sherpa/CarrierSherpaApplication.java
@@ -0,0 +1,13 @@
+package com.sherpa.carrier_sherpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrierSherpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrierSherpaApplication.class, args);
+	}
+
+}
---
+++ carrier_sherpa/src/test/java/com/sherpa/carrier_sherpa/CarrierSherpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sherpa.carrier_sherpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrierSherpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
