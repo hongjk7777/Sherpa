cdf6e23105fd51ab8639c95e4701adf6c005c9bf
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-10-15 20:06:49 +0900

    [Add] add find member by email

--- carrier_sherpa/src/main/java/com/sherpa/carrier_sherpa/domain/repository/MemberRepository.java
+++ carrier_sherpa/src/main/java/com/sherpa/carrier_sherpa/domain/repository/MemberRepository.java
@@ -1,2 +1,9 @@
-package com.sherpa.carrier_sherpa.domain.Repository;public class MemberRepository {
+package com.sherpa.carrier_sherpa.domain.repository;
+
+import com.sherpa.carrier_sherpa.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmail(String email);
 }
