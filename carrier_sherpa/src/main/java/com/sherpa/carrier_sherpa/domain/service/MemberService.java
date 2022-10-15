93771dfa48741d28b19475fe4c48e71da72aab12
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-10-15 20:07:47 +0900

    [Add] add sign up service function and test

--- carrier_sherpa/src/main/java/com/sherpa/carrier_sherpa/domain/service/MemberService.java
+++ carrier_sherpa/src/main/java/com/sherpa/carrier_sherpa/domain/service/MemberService.java
@@ -1,2 +1,27 @@
-package com.sherpa.carrier_sherpa.domain.Service;public class MemberService {
+package com.sherpa.carrier_sherpa.domain.service;
+
+import com.sherpa.carrier_sherpa.domain.entity.Member;
+import com.sherpa.carrier_sherpa.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if (findMember != null) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
 }
--- carrier_sherpa/src/test/java/com/sherpa/carrier_sherpa/domain/service/MemberServiceTest.java
+++ carrier_sherpa/src/test/java/com/sherpa/carrier_sherpa/domain/service/MemberServiceTest.java
@@ -1,4 +1,41 @@
+package com.sherpa.carrier_sherpa.domain.service;
+
+import com.sherpa.carrier_sherpa.domain.entity.Member;
+import com.sherpa.carrier_sherpa.dto.MemberFormDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
 class MemberServiceTest {
-  
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember() {
+        MemberFormDto memberFormDto = MemberFormDto.builder()
+                .email("test@email.com")
+                .password("1111")
+                .build();
+
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest() {
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+
+        assertEquals(member.getEmail(), savedMember.getEmail());
+    }
 }
